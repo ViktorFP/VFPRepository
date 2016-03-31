140a3335df700491b7e08bfece956f6b2c6c04d4
ViktorFP <VikPr_85@mail.ru>
2016-03-31 22:31:48 +0300

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -9,8 +9,8 @@ public class Runner {
 
 		String name="newName";
 		Bean2 bean21 = new Bean2();
-		Bean2 bean22 = new Bean2(1, "uniqueName", 2.2);
-		Bean2 bean23 = new Bean2(2, name, 2.2);
+		Bean2 bean22 = new Bean2(1, new String("123"), 2.2);
+		Bean2 bean23 = new Bean2(2, new String("123"), 2.2);
 		Bean2 bean24 = new Bean2(2, name, 2.4);
 
 		Object[] objects = { bean11, bean12, bean21, bean22, bean23, bean24 };
@@ -25,9 +25,9 @@ public class Runner {
 
 				String message = "The objects " + i + " and " + j;
 				if (Analyzer.equalObjects(objects[i], objects[j])) {
-					System.out.println(message + " is equals");
+					System.out.println(message + " are equals");
 				} else {
-					System.out.println(message + " isn't equals");
+					System.out.println(message + " aren't equals");
 				}
 			}
 		}
